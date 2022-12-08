70a24e15e9dbf4797425e6185010e23787b401bd
artemarif <105998022+artemarif@users.noreply.github.com>
2022-12-08 15:53:01 +0300

    Экземпляр от интерфейса

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl udi = new UserDaoJDBCImpl();
+    UserDao udi = new UserDaoJDBCImpl();
     public void createUsersTable() {
         udi.createUsersTable();
     }
